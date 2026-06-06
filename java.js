ed9cfff66a70a71f8fa66595467a7139e5759203
nhandeptrai110 <nhanmetoantb9phay9@gmail.com>
2026-06-06 12:51:42 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
 // === ĐỒNG HỒ ĐẾM NGƯỢC ===
-let total-seconds=90*60
+let totalSeconds=90*60
 const countdownElement=document.getElementById('countdown')
 
 const timerInterval=setInterval(() => {
-    let minutes=Math.floor(total-seconds/60);
-    let seconds=total-seconds%60;
+    let minutes=Math.floor(totalSeconds/60);
+    let seconds=totalSeconds%60;
 
     minutes=minutes<10 ? '0'+minutes:minutes;
     seconds=seconds<10 ? '0'+seconds:seconds;
